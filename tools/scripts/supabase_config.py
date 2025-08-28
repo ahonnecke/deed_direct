f5df151aa8315aa4e779987bdc49bd33493848cb
Ashton Honnecke <ashton@pixelstub.com>
2025-08-28 09:28:29 -0600

    unset org id

--- tools/scripts/supabase_config.py
+++ tools/scripts/supabase_config.py
@@ -8,7 +8,7 @@ It is used by various scripts to ensure consistent settings across the project.
 
 # Default Supabase organization ID
 # This is used when creating new Supabase projects
-SUPABASE_ORG_ID = "wtzdspvojbntegninaxc"
+SUPABASE_ORG_ID = ""
 
 # Default region for Supabase projects
 SUPABASE_REGION = "us-west-1"
